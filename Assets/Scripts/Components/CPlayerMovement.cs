ebcd23901f260f2b18e30856c77281d736dd6937
Dan Singer (RIT Student) <djs5435@ad.rit.edu>
2019-10-12 15:51:26 -0400

    Rough ECS player controller implementation

--- Assets/Scripts/Components/CPlayerMovement.cs
+++ Assets/Scripts/Components/CPlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;   
 
-public struct CPlayerMovement : IComponentData
+public class CPlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public float rotationSpeed;
--- Assets/Scripts/Systems/SPlayer.cs
+++ Assets/Scripts/Systems/SPlayer.cs
@@ -9,9 +9,23 @@ public class SPlayer : ComponentSystem
 {
     protected override void OnUpdate()
     {
-        Entities.ForEach((ref Translation translation, ref Rotation rotation, ref CPlayerMovement playerMovement) =>
-        {
-            
+        float horz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        Entities.ForEach((Entity entity, Transform transform, CharacterController controller, CPlayerMovement playerMovement, CPitchYaw pitchYaw) => {
+            // Translation
+            Vector3 localVel = new Vector3(horz, 0, vert);
+            Vector3 worldVel = Quaternion.Euler(0, transform.eulerAngles.y, 0) * localVel;
+            worldVel *= playerMovement.moveSpeed;
+            controller.Move(worldVel * Time.deltaTime);
+
+            // Rotation
+            float yaw = Input.GetAxis("Mouse X");
+            float pitch = Input.GetAxis("Mouse Y");
+
+            pitchYaw.pitch += -pitch * playerMovement.rotationSpeed * Time.deltaTime;
+            pitchYaw.yaw += yaw * playerMovement.rotationSpeed * Time.deltaTime;
+
+            transform.rotation = Quaternion.Euler(pitchYaw.pitch, pitchYaw.yaw, 0);
         });
     }
 }
