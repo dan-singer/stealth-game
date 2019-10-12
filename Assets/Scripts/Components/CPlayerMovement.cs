8b8ed3d7b160b62653f2824919b8f3e31a3e3d59
Dan Singer (RIT Student) <djs5435@ad.rit.edu>
2019-10-12 16:09:50 -0400

    Clean up and document player controller

--- Assets/Scripts/Components/CPlayerMovement.cs
+++ Assets/Scripts/Components/CPlayerMovement.cs
@@ -3,8 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;   
 
+/// <summary>
+/// Player Movement Settings
+/// </summary>
 public class CPlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public float rotationSpeed;
+
+    [HideInInspector]
+    public float pitch = 0;
+    [HideInInspector]
+    public float yaw = 0;
+
+    public float minPitch = -45.0f;
+    public float maxPitch = 45.0f;
 }
--- Assets/Scripts/Systems/SPlayerMovement.cs
+++ Assets/Scripts/Systems/SPlayerMovement.cs
@@ -5,13 +5,16 @@ using Unity.Transforms;
 using UnityEngine;
 using Unity.Mathematics;
 
-public class SPlayer : ComponentSystem
+/// <summary>
+/// Player Movement System. Handles Translation and Rotation for the First Person character
+/// </summary>
+public class SPlayerMovement : ComponentSystem
 {
     protected override void OnUpdate()
     {
         float horz = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
-        Entities.ForEach((Entity entity, Transform transform, CharacterController controller, CPlayerMovement playerMovement, CPitchYaw pitchYaw) => {
+        Entities.ForEach((Entity entity, Transform transform, CharacterController controller, CPlayerMovement playerMovement) => {
             // Translation
             Vector3 localVel = new Vector3(horz, 0, vert);
             Vector3 worldVel = Quaternion.Euler(0, transform.eulerAngles.y, 0) * localVel;
@@ -22,10 +25,11 @@ public class SPlayer : ComponentSystem
             float yaw = Input.GetAxis("Mouse X");
             float pitch = Input.GetAxis("Mouse Y");
 
-            pitchYaw.pitch += -pitch * playerMovement.rotationSpeed * Time.deltaTime;
-            pitchYaw.yaw += yaw * playerMovement.rotationSpeed * Time.deltaTime;
+            playerMovement.pitch += -pitch * playerMovement.rotationSpeed * Time.deltaTime;
+            playerMovement.pitch = Mathf.Clamp(playerMovement.pitch, playerMovement.minPitch, playerMovement.maxPitch);
+            playerMovement.yaw += yaw * playerMovement.rotationSpeed * Time.deltaTime;
 
-            transform.rotation = Quaternion.Euler(pitchYaw.pitch, pitchYaw.yaw, 0);
+            transform.rotation = Quaternion.Euler(playerMovement.pitch, playerMovement.yaw, 0);
         });
     }
 }
