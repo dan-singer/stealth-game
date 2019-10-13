776ed5d9b9da32720d500b64d3d0484c87812c83
Nick Marano (RIT Student) <ngm9939@ad.rit.edu>
2019-10-12 20:35:41 -0400

    [Cut temporary errors out]

--- Assets/Scripts/RandomSpawning.cs
+++ Assets/Scripts/RandomSpawning.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class RandomSpawning : MonoBehaviour
 {
     // [SerializedField]
-    public Dictionary<GameObject[], bool> spawnLocs;
-    private bool spawnIsActive = false;
+    // public Dictionary<GameObject[], bool> spawnLocs;
+    // private bool spawnIsActive = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
         // loop through predetermined spawn points
-        for(int i = 0; i < (spawnLocs.Count / 2); i++)
+        /*for(int i = 0; i < (spawnLocs.Count / 2); i++)
         {
             // select a random one  *This will need to be revised, potential for crash to occurr
-            int rand = Mathf.Random() * 6;
+            // int rand = Mathf.Random() * 6;
             // set the value of dictionary to true, enabling ocation as a valid spawn point
             spawnLocs[rand] = true;
         }
-
+        */
         // spawn the enemies
 
     }
